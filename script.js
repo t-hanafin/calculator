c1048bcbbc8d16bd179a3308d0af218483e3eaeb
t-hanafin <thanafin@outlook.com>
2023-02-18 12:27:17 -0500

    add minor changes, add functions for the plus-minus and percentage buttons

--- script.js
+++ script.js
@@ -1,112 +1,149 @@
 const display = document.querySelector('.display');
 var displayValue = 0;
 display.textContent = displayValue;
-var firstValue = undefined;
-var secondValue = undefined;
+var firstValue = null;
+let secondValue = null;
 let operator;
-let result = null;
+var result = null;
+operatorLastPressed = false;
+equalsLastPressed = false;
+
+// Receives function call from a button, calls subsequent function based on buttonID.
+
+function buttonPressed(buttonId) {
+    if (buttonId >= 0 && buttonId < 10) {
+        numberPressed(buttonId);
+    } else if (buttonId === "clear") { 
+        clear();
+    } else if (buttonId === "add" || buttonId === "subtract" || buttonId === "divide" || buttonId === "multiply") { 
+        operatorPressed(buttonId);
+    } else if (buttonId === ".") {
+        decimalPressed();
+    } else if (buttonId === "equals") {
+        equalsPressed();
+    } else if (buttonId === "+/-") {
+        plusMinusPressed();
+    } else if (buttonId === "%") {
+        percentPressed();
+    }
+}
+
+// Button-press functions.
+
+function clear() {
+    displayValue = 0;
+    updateDisplay(displayValue);
+    firstValue = undefined;
+    operator = undefined;
+    result = null;
+    operatorLastPressed = false;
+    equalsLastPressed = false;
+}
+
+function operatorPressed(buttonId) {
+    firstValue = displayValue;
+    operator = buttonId;
+    operatorLastPressed = true;
+    equalsLastPressed = false;
+}
+
+function numberPressed(buttonId) {
+    if (operatorLastPressed === true || displayValue === 0) {
+        displayValue = buttonId;
+        updateDisplay(displayValue);
+        operatorLastPressed = false;
+    } else {
+        displayValue += buttonId;
+        updateDisplay(displayValue);
+    }
+    operatorLastPressed = false;
+    equalsLastPressed = false;
+}
+
+function decimalPressed() {
+    operatorLastPressed = false;
+    equalsLastPressed = false;
+    let addDecimal = displayValue.includes(".");
+    if (addDecimal == false) {
+        displayValue += ".";
+        updateDisplay(displayValue);
+    }
+}
+
+function equalsPressed() {
+    operatorLastPressed = false;
+    if (equalsLastPressed === false) {
+        equalsLastPressed = true;
+        secondValue = displayValue;
+        operate(operator, firstValue, secondValue);
+    } else if (equalsLastPressed === true) {
+        firstValue = displayValue;
+        operate(operator, firstValue, secondValue);
+    }
+    equalsLastPressed = true;
+}
+
+function plusMinusPressed() {
+    displayValue = parseFloat(displayValue) * -1;
+    updateDisplay(displayValue);
+}
+
+function percentPressed() {
+    displayValue = parseFloat(displayValue) * 0.01;
+    updateDisplay(displayValue);
+}
+
+// Updates display.
+
+function updateDisplay(displayValue) {
+    display.textContent = displayValue;
+}
+
+// Mathematical functions.
 
 function add(firstValue, secondValue) {
-    return result = firstValue + secondValue;
+    return result = parseFloat(firstValue) + parseFloat(secondValue);
 }
 
 function subtract(firstValue, secondValue) {
-    return result = firstValue - secondValue;
+    return result = parseFloat(firstValue) - parseFloat(secondValue);
 }
 
 function multiply(firstValue, secondValue) {
-    return result = firstValue * secondValue;
+    return result = parseFloat(firstValue) * parseFloat(secondValue);
 }
 
 function divide(firstValue, secondValue) {
     // First checks to see if the divisor is zero, chastises the user if so. 
-    if (secondValue == 0) {
-        return result = "You know better than to divide by zero. Use your head.";
+    if (secondValue === 0) {
+        alert("You know better than to divide by zero. Use your head.");
+        return result = 0;
     } else {
-        return result = firstValue / secondValue;
+        console.log(firstValue, secondValue);
+        return result = parseFloat(firstValue) / parseFloat(secondValue);
     }
 }
+
+
+// Performs operation, updates display with result.
 
 function operate(operator, firstValue, secondValue) {
     switch(operator) {
         case "add":
-            result = displayValue = add(firstValue, secondValue);
-            updateDisplay(result);
+            displayValue = add(firstValue, secondValue);
+            updateDisplay(displayValue);
             break;
         case "subtract":
-            result = subtract(firstValue, secondValue);
-            updateDisplay(result);
+            displayValue = subtract(firstValue, secondValue);
+            updateDisplay(displayValue);
             break;
         case "multiply":
-            result = multiply(firstValue, secondValue);
-            updateDisplay(result);
+            displayValue = multiply(firstValue, secondValue);
+            updateDisplay(displayValue);
             break;
         case "divide": 
-            result = divide(firstValue, secondValue);
-            updateDisplay(result);
+            displayValue = divide(firstValue, secondValue);
+            updateDisplay(displayValue);
             break;
-        default: 
-            break;
-    }
-}
-
-function updateDisplay(buttonId) {
-    if (firstValue === undefined) {
-        if (buttonId == "clear") {
-            displayValue = 0;
-            operator = undefined;
-            result = null;
-        } else if (buttonId == "zero" && display.textContent == 0) {
-            displayValue = 0;
-        } else if (display.textContent == 0 && buttonId != "zero") {
-            displayValue = buttonId;
-        } else if (buttonId == "." && displayValue.includes(".")) {
-            console.log("this message is here because the decimal button was pressed more than once and i don't know how to make javascript just do nothing yet");
-        } else {
-            displayValue += buttonId;
-        }
-    } else if (firstValue != undefined) {
-        displayValue = 0;
-        if (buttonId == "clear") {
-            displayValue = 0;
-            firstValue = undefined;
-            operator = undefined;
-            result = null;
-        } else if (buttonId == "zero" && display.textContent == 0) {
-            displayValue = 0;
-        } else if (display.textContent == 0 && buttonId != "zero") {
-            displayValue = buttonId;
-            secondValue = buttonId;
-        } else if (buttonId == "." && displayValue.includes(".")) {
-            console.log("this message is here because the decimal button was pressed more than once and i don't know how to make javascript just do nothing yet");
-        } else {
-            displayValue += buttonId;
-        }
-    }
-    display.textContent = displayValue;
-}
-
-function operatorEntered(buttonId) {
-    firstValue = displayValue;
-    operator = buttonId;
-}
-
-function equals() {
-    secondValue = displayValue;
-    console.log(operator);
-    operate(operator, firstValue, secondValue);
-}
-
-function clear(buttonId) {
-    if (buttonId = "clear") {
-        console.log(displayValue, firstValue, secondValue, operator, result);
-        displayValue = 0;
-        firstValue = undefined;
-        secondValue = undefined;
-        operator = undefined;
-        result = undefined;
-        console.log(displayValue, firstValue, secondValue, operator, result);        
-    } else {
-        console.log("this is weird");
     }
 }

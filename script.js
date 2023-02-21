de472895ce3c271cd1f6d1404f2f78a26751ccc5
t-hanafin <thanafin@outlook.com>
2023-02-21 15:50:04 -0500

    switch input mode from a function call from the html button to an event listener added to the buttons in javascript. improve comment details.

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 const display = document.querySelector('.display');
-const buttons = document.getElementsByTagName("button");
-const clearButton = document.getElementById("clear");
+const buttons = document.querySelectorAll('.buttons');
+const clearButton = document.getElementById('clear');
 let displayValue = 0;
 display.textContent = displayValue;
 var firstOperand = undefined;
 let secondOperand = undefined;
-let operator = "";
+let operator = '';
 var result = undefined;
 var equalsActive = false;
 var operatorActive = false;
 
-// Receives function call from an HTML button, calls subsequent function based on buttonID.
+
+// Listens for click events on the HTML buttons, 
+// calls subsequent function based on buttonID.
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        buttonId = e.target.id;
+        buttonPress(buttonId);
+    })
+})
 
 function buttonPress(buttonId) {
-    if (buttonId == "zerozero") {
+    if (buttonId == 'zerozero') {
         zeroZeroPress();
-    } else if (buttonId == "clear") { 
+    } else if (buttonId == 'clear') { 
         clear();
-    } else if (buttonId == "add" || buttonId === "subtract" || buttonId === "divide" || buttonId === "multiply") { 
+    } else if (
+            buttonId == 'add' || 
+            buttonId === 'subtract' || 
+            buttonId === 'divide' || 
+            buttonId === 'multiply') { 
         operatorPress(buttonId);
-    } else if (buttonId == ".") {
+    } else if (buttonId == '.') {
         decimalPress();
-    } else if (buttonId == "equals") {
-        if (operator != "") { // This means the = button will do nothing if there is no operator entered. 
-            equalsPress();
-        }
-    } else if (buttonId === "+/-") {
+    } else if (buttonId == 'equals') {
+        equalsPress();
+    } else if (buttonId === '+/-') {
         plusMinusPress();
-    } else if (buttonId === "%") {
+    } else if (buttonId === '%') {
         percentPress();
-    } else if (buttonId >= 0 && buttonId < 10) {
+    } else if (parseInt(buttonId) >= 0 && parseInt(buttonId) < 10) {
         numberPress(buttonId);
     }
 }
@@ -41,18 +52,19 @@ function clear() {
     updateDisplay(displayValue);
     firstOperand = undefined;
     secondOperand = undefined;
-    operator = undefined;
+    operator = '';
     result = undefined;
     equalsActive = false;
     operatorActive = false;
-    display.style.cssText += "font-size: 30px;padding: 7px;";
+    display.style.cssText += 'font-size: 30px;padding: 7px;';
     for (const button of buttons) {
         button.disabled = false;
     }
 }
 
 function numberPress(buttonId) {
-    if (operatorActive === true || displayValue == 0) {
+    console.log(operatorActive, equalsActive);
+    if (operatorActive == true || displayValue == 0) {
         displayValue = buttonId;
         updateDisplay(displayValue);
         operatorActive = false;
@@ -60,24 +72,25 @@ function numberPress(buttonId) {
         displayValue += buttonId;
         updateDisplay(displayValue);
     }
+    console.log(firstOperand, secondOperand);
 }
 
 function decimalPress() {
-    if (!displayValue.toString().includes(".")) {
-        displayValue += ".";
+    if (!displayValue.toString().includes('.')) {
+        displayValue += '.';
         updateDisplay(displayValue);
     }
 }
 
 function plusMinusPress() {
-    operator = "multiply";
+    operator = 'multiply';
     firstOperand = displayValue;
     secondOperand = -1;
     operate(operator, firstOperand, secondOperand);
 }
 
 function percentPress() {
-    operator = "multiply";
+    operator = 'multiply';
     firstOperand = displayValue;
     secondOperand = 0.01;
     operate(operator, firstOperand, secondOperand);
@@ -89,26 +102,30 @@ function zeroZeroPress() {
 }
 
 // The equals button has two modes: 
-// In the first, the equals button performs an operation on the two operands entered. 
-// In the second, the equals button continues the last operation using the last operand entered and the previous result. 
+// In the first, the equals button performs an operation on the two 
+// operands entered. In the second, the equals button continues the
+// last operation using the last operand entered and the previous result. 
 
 function equalsPress() {
-    if ((equalsActive === false) && (operator != "")) {
+    if (equalsActive == false && operator != '') {
         equalsActive = true;
         secondOperand = displayValue;
         operate(operator, firstOperand, secondOperand);
         displayValue = result;
         updateDisplay(displayValue);
-    } else if ((equalsActive === true) && (operator != "")) {
+    } else if (equalsActive == true && operator != '') {
         firstOperand = displayValue;
         operate(operator, firstOperand, secondOperand);
         displayValue = result;
         updateDisplay(displayValue);
     }
+    console.log(firstOperand, secondOperand);
 }
 
-// The operator buttons have three modes: the first time one is pressed, it writes the displayed value to the firstOperand variable.
-// The second mode, if there hasn't been a second number entered, it performs the given operation on the one number entered (i.e. 1 ++ will result in 2).
+// The operator buttons have three modes: the first time one is pressed, it 
+// writes the displayed value to the firstOperand variable. The second mode, 
+// if there hasn't been a second number entered, it performs the given
+// operation on the one number entered (i.e. 1 ++ will result in 2).
 // The third mode allows for continuous operations with a running total. 
 
 function operatorPress(buttonId) {
@@ -122,8 +139,11 @@ function operatorPress(buttonId) {
         secondOperand = displayValue;
     }
     if (operatorActive) {
-        updateDisplay(operate(operator, firstOperand, secondOperand)); // Performs an operation on the second operator-button press if there's only been one operand entered. 
-    } else if (!isNaN(operate(operator, firstOperand, secondOperand))) { // Does the math if there are two operands. 
+        // Performs an operation on the second operator-button press 
+        // if there's only been one operand entered. 
+        updateDisplay(operate(operator, firstOperand, secondOperand)); 
+    } else if (!isNaN(operate(operator, firstOperand, secondOperand))) { 
+        // Does the math if there are two operands. 
         operate(operator, firstOperand, secondOperand);
         displayValue = result;
         updateDisplay(displayValue);
@@ -133,15 +153,16 @@ function operatorPress(buttonId) {
         operatorActive = true;
     }
     operator = buttonId;
+    console.log(firstOperand, secondOperand);
 }
 
 // Updates display, shows error if the value is too long.
 
 function updateDisplay(displayValue) {
-    if (displayValue.toString().includes("+")) {
+    if (displayValue.toString().includes('+')) {
         error();
     } else if (displayValue.toString().length > 10) {
-        display.style.cssText += "font-size: 10px;padding: 18.5px;";
+        display.style.cssText += 'font-size: 10px;padding: 18.5px;';
         display.textContent = displayValue
     } else {
         display.textContent = displayValue;
@@ -149,8 +170,8 @@ function updateDisplay(displayValue) {
 }
 
 function error() {
-    display.style.cssText += "font-size: 30px;padding: 7px;";
-    displayValue = "error";
+    display.style.cssText += 'font-size: 30px;padding: 7px;';
+    displayValue = 'error';
     display.textContent = displayValue;
     for (const button of buttons) {
         button.disabled = true;
@@ -173,9 +194,10 @@ function multiply(firstOperand, secondOperand) {
 }
 
 function divide(firstOperand, secondOperand) {
-    // First checks to see if the divisor is zero, chastises the user and returns 'error' if so. 
+    // First checks to see if the divisor is zero, chastises the user and 
+    // returns 'error' if so. 
     if (secondOperand == 0) {
-        alert("You know better than to divide by zero. Use your head next time.");
+        alert('Don\'t divide by zero. Use your head next time.');
         firstOperand = undefined;
         secondOperand = undefined;
         return result = 0;
@@ -188,17 +210,18 @@ function divide(firstOperand, secondOperand) {
 
 function operate(operator, firstOperand, secondOperand) {
     switch(operator) {
-        case "add":
+        case 'add':
             return result = add(firstOperand, secondOperand);
             break;
-        case "subtract":
+        case 'subtract':
             return result = subtract(firstOperand, secondOperand);
             break;
-        case "multiply":
+        case 'multiply':
             return result = multiply(firstOperand, secondOperand);
             break;
-        case "divide": 
+        case 'divide': 
             return result = divide(firstOperand, secondOperand);
             break;
     }
 }
+
